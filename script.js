52cf8909b0e9b7f5332517e07d1bf2147c78dad0
Diego <dvalderramacalderon@gmail.com>
2023-06-05 06:06:28 -0500

    clase fina terminada

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-(() => {
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
+
 const btn = document.querySelector("[data-form-btn]");
 
 const createTask = (evento) =>{
@@ -29,30 +31,7 @@ const createTask = (evento) =>{
 
 btn.addEventListener('click', createTask);
 
-const checkComplete = () => {
-  const i = document.createElement('i');
-  i.classList.add('far','fa-check-square','icon');
-  i.addEventListener("click",completeTask);
-  return i;
 
-};
-// immediately invokes function expression IIFE
-const completeTask = (event) => {
-  const element = event.target
-  element.classList.toggle ("fas");
-  element.classList.toggle ("completeIcon");
-  element.classList.toggle ("far");
-};
 
-const deleteIcon=() => {
-  const i = document.createElement("i");
-  i.classList.add("fas","fa-trash-alt","trashIcon","icon");
- i.addEventListener("click", deleteTask);
-  return i;
-}
 
-const deleteTask = () =>{
-  console.log("Eliminar tarea")
-}
 
-})();
